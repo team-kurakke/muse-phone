fdf9235d67d72ba1c749322385060f636a4cec97
saezuri510 <saezuru510@gmail.com>
2023-09-05 21:58:36 +0900

    feat: package.jsonにscriptを追加しました

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
   mode: "jit",
+  plugins: [],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
